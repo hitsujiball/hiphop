790f75706e1218317d514cd4ed6efb8b515d3dfc
Hattori Miho <hattori@un-t.com>
2019-11-14 09:43:43 +0900

    generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,13 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    // title: process.env.npm_package_name || '',
+    title: 'HIPHOP用語変換ツール',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'robots', name: 'robots', content: 'noindex' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
